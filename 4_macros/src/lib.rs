aded7088dad833de45d878cde4ff69f5aff0e402
Jocelyn Facchini <jocelyn.facchini@codurance.com>
2023-01-26 22:08:19 +0000

    Add more test cases

--- 4_macros/src/lib.rs
+++ 4_macros/src/lib.rs
@@ -40,6 +40,58 @@ mod tests {
     // }
 
     // #[test]
+    // fn parse_a_valid_string() {
+    //     assert_eq!(json!("Hello"), Json::String(String::from("Hello")));
+    // }
+
+    // #[test]
+    // fn parse_a_valid_array() {
+    //     assert_eq!(
+    //         json!(["a", "b", "c"]),
+    //         Json::Array(vec![
+    //             Json::String(String::from("a")),
+    //             Json::String(String::from("b")),
+    //             Json::String(String::from("c")),
+    //         ])
+    //     );
+    // }
+
+    // #[test]
+    // fn parse_a_valid_array_of_arrays() {
+    //     assert_eq!(
+    //         json!([["a", "b"], ["c"]]),
+    //         Json::Array(vec![
+    //             Json::Array(vec![
+    //                 Json::String(String::from("a")),
+    //                 Json::String(String::from("b"))
+    //             ]),
+    //             Json::Array(vec![Json::String(String::from("c"))]),
+    //         ])
+    //     );
+    // }
+
+    // #[test]
+    // fn parse_a_valid_object() {
+    //     assert_eq!(json!({
+    //         "Hello": "world",
+    //         "Test": 1,
+    //         "Names": [
+    //             "John",
+    //             "Doe"
+    //         ]
+    //     }), Json::Object(
+    //         vec![
+    //             (String::from("Hello"), Json::String(String::from("world"))),
+    //             (String::from("Test"), Json::Number(1.0)),
+    //             (String::from("Names"), Json::Array(vec![
+    //                 Json::String(String::from("John")),
+    //                 Json::String(String::from("Doe")),
+    //             ])),
+    //         ].into_iter().collect()
+    //     ));
+    // }
+
+    // #[test]
     // fn parse_a_valid_number() {
     //     assert_eq!(json!(1), Json::Number(1.0));
     //     assert_eq!(json!(1u8), Json::Number(1.0));
@@ -58,33 +110,7 @@ mod tests {
 
     // #[test]
     // fn parse_a_valid_float() {
-    //     assert_eq!(json!(1.0), Json::Number(1.0));
-    // }
-
-    // #[test]
-    // fn parse_a_valid_string() {
-    //     assert_eq!(json!("Hello"), Json::String(String::from("Hello")));
-    // }
-
-    // #[test]
-    // fn parse_a_valid_array() {
-    //     assert_eq!(json!(["a", "b", "c"]), Json::Array(vec![
-    //         Json::String(String::from("a")),
-    //         Json::String(String::from("b")),
-    //         Json::String(String::from("c")),
-    //     ]));
-    // }
-
-    // #[test]
-    // fn parse_a_valid_object() {
-    //     assert_eq!(json!({
-    //         "Hello": "world",
-    //         "Test": 1
-    //     }), Json::Object(
-    //         vec![
-    //             (String::from("Hello"), Json::String(String::from("world"))),
-    //             (String::from("Test"), Json::Number(1.0)),
-    //         ].into_iter().collect()
-    //     ));
+    //     assert_eq!(json!(1.0f32), Json::Number(1.0));
+    //     assert_eq!(json!(1.0f64), Json::Number(1.0));
     // }
 }
